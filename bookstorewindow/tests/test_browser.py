eb573d0152687854e9c47f49dddc4ee5874754fd
Pierce Edmiston <pierce.edmiston@gmail.com>
2019-01-16 13:01:37 -0600

    Display an input element on the homepage
    Passes the first function test expecting there to be
    a search input on the homepage.
    
    Renders the "window/templates/window/index.html" template.
    
    Includes the "window" app in settings.py so that the
    template discovery works properly.
    
    Fixes a bug in the selenium test, using the ".get_attribute"
    method on web elements.

--- bookstorewindow/tests/test_browser.py
+++ bookstorewindow/tests/test_browser.py
@@ -12,4 +12,4 @@ class BrowserTest(StaticLiveServerTestCase):
     def test_window_has_search_input(self):
         self.browser.get(self.live_server_url)
         input = self.browser.find_element_by_tag_name("input")
-        self.assertEquals(input.name, "search")
+        self.assertEquals(input.get_attribute("name"), "search")
