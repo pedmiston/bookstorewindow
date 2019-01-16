7a88ea1818d43d0a4d6c0e2e2249d118000da1bd
Pierce Edmiston <pierce.edmiston@gmail.com>
2019-01-16 14:48:23 -0600

    Add a failing user test
    The test expects a user to be able to search for "The Bible" and get
    more than 0 results.
    
    I refactored the test set up so that the browser is only booted
    once for all tests, and the url is changed between tests.

--- bookstorewindow/tests/test_browser.py
+++ bookstorewindow/tests/test_browser.py
@@ -3,13 +3,26 @@ from selenium import webdriver
 
 
 class BrowserTest(StaticLiveServerTestCase):
-    def setUp(self):
-        self.browser = webdriver.Firefox()
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.browser = webdriver.Firefox()
 
-    def tearDown(self):
-        self.browser.quit()
+    @classmethod
+    def tearDownClass(cls):
+        cls.browser.quit()
+        super().tearDownClass()
+
+    def setUp(self):
+        self.browser.get(self.live_server_url)
 
     def test_window_has_search_input(self):
-        self.browser.get(self.live_server_url)
-        input = self.browser.find_element_by_tag_name("input")
-        self.assertEquals(input.get_attribute("name"), "search")
+        self.browser.find_element_by_id("id_query")
+
+    def test_user_can_search_for_the_bible(self):
+        search = self.browser.find_element_by_id("id_query")
+        search.send_keys("The Bible")
+        search.submit()
+
+        results = self.browser.find_elements_by_tag_name("li")
+        assert len(results) > 0
