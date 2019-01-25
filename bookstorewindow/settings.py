16393d9b7271aca110b911ae90fd0d2ba67fc54c
Pierce Edmiston <pierce.edmiston@gmail.com>
2019-01-25 10:51:28 -0600

    Use Django-Heroku to insert config vars

--- bookstorewindow/settings.py
+++ bookstorewindow/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
+import django_heroku
+
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -116,3 +119,6 @@ STATIC_URL = "/static/"
 
 
 CRISPY_TEMPLATE_PACK = "bootstrap4"
+
+# Activate Django-Heroku
+django_heroku.settings(local())
