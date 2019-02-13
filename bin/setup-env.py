e921c5963a3f927c7422586f27137e23ad5d1ee5
Pierce Edmiston <pierce.edmiston@gmail.com>
2019-02-13 16:42:38 -0600

    Move env template to separate file, "env.j2"
    The purpose here is to make it more explicit what environment
    variables are expected and required.

--- bin/setup-env.py
+++ bin/setup-env.py
@@ -3,17 +3,10 @@
 import os
 import jinja2
 
-template = jinja2.Template(
-    """\
-GOOGLE_API_KEY={{ google_api_key }}
-DEBUG=1
-"""
-)
-
+template = jinja2.Template(open("env.j2").read())
 
 if __name__ == "__main__":
     env_vars = {}
     env_vars["google_api_key"] = input("Enter the Google API Key: ")
-
     with open(".env", "w") as dst:
         dst.write(template.render(**env_vars))
